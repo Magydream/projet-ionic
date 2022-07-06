df750a8ca4265ac7e32fd8bd6bba7f7476f8a33f
Magydream <87599508+Magydream@users.noreply.github.com>
2022-07-07 01:20:47 +0200

    init firebase + auth service et avatar service

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'projetionic-d0002',
+    appId: '1:884779162188:web:692bbe31941e0a04b132e9',
+    storageBucket: 'projetionic-d0002.appspot.com',
+    locationId: 'europe-central2',
+    apiKey: 'AIzaSyCrbOHEuYRISnOHn9ufogTHTE_6ELnhLeU',
+    authDomain: 'projetionic-d0002.firebaseapp.com',
+    messagingSenderId: '884779162188',
+    measurementId: 'G-E3631SB27B',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'projetionic-d0002',
+    appId: '1:884779162188:web:692bbe31941e0a04b132e9',
+    storageBucket: 'projetionic-d0002.appspot.com',
+    locationId: 'europe-central2',
+    apiKey: 'AIzaSyCrbOHEuYRISnOHn9ufogTHTE_6ELnhLeU',
+    authDomain: 'projetionic-d0002.firebaseapp.com',
+    messagingSenderId: '884779162188',
+    measurementId: 'G-E3631SB27B',
+  },
   production: false,
   apiKey:'5049f830c613d1d67ab1998c4b8fa53e',
   apiUrl:'https://api.themoviedb.org/3/',
